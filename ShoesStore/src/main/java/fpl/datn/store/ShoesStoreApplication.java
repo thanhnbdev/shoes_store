ce91acd63e09a57652cf6ceb7d703ecff9bc66ad
thanhnbdev <thanhnb.dev@gmail.com>
2022-09-29 20:50:08 +0700

    project started

---
+++ ShoesStore/src/main/java/fpl/datn/store/ShoesStoreApplication.java
@@ -0,0 +1,13 @@
+package fpl.datn.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesStoreApplication.class, args);
+	}
+
+}
